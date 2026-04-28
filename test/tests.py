abf168d5dfbe540549725be17a1e75551de54747
Lucas Coelho <lucasvgcoelho@gmail.com>
2026-04-27 23:19:18 -0300

    add asyncio to test funcs

--- test/tests.py
+++ test/tests.py
@@ -10,28 +10,34 @@ async def test_root():
     result = await root()
     assert result == {"message": "Hello World"}
 
+@pytest.mark.asyncio
 async def  test_lucas():
     with patch('random.randint', return_value=12345):
         result = await lucas()
     assert result == {"Test": True, "num_aleatorio": 12345}
 
+@pytest.mark.asyncio
 async def  test_estudante_cadastro():
     estudante_teste = Estudante(nome="Nome", curso="Curso", ativo=False)
     result = await estudante_cadastro(estudante_teste)
     assert estudante_cadastro == result
 
+@pytest.mark.asyncio
 async def  test_estudante_update_negativo():
     result = await estudante_update(-5)
     assert not result
 
+@pytest.mark.asyncio
 async def  test_estudante_update_positivo():
     result = await estudante_update(10)
     assert result
 
+@pytest.mark.asyncio
 async def  test_estudante_delete_negativo():
     result = await estudante_delete(-5)
     assert not result
 
+@pytest.mark.asyncio
 async def  test_estudante_delete_positivo():
     result = await estudante_delete(10)
     assert result
