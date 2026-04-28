2b19df9f480dd5cc5fd138abcc9ecd6f2f1e4c62
Lucas Coelho <lucasvgcoelho@gmail.com>
2026-04-27 23:09:45 -0300

    Update tests.py

--- test/tests.py
+++ test/tests.py
@@ -1,28 +1,41 @@
+from unittest import result
+
 from src.main import *
 from unittest.mock import patch
 
-
 def test_root():
-    assert root() == {"message": "Hello World"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello World"}
 
 def test_lucas():
     with patch('random.randint', return_value=12345):
-        result = lucas();
+        result = lucas()
+        yield result
     assert result == {"Test": True, "num_aleatorio": 12345}
 
 def test_estudante_cadastro():
     estudante_teste = Estudante(nome="Nome", curso="Curso", ativo=False)
-    assert estudante_teste == estudante_cadastro(estudante_teste)
+    result = estudante_cadastro(estudante_teste)
+    yield result
+    assert estudante_cadastro == result
 
 def test_estudante_update_negativo():
-    assert not estudante_update(-5)
+    result = estudante_update(-5)
+    yield result
+    assert not result
 
 def test_estudante_update_positivo():
-    assert estudante_update(10)
+    result = estudante_update(10)
+    yield result
+    assert result
 
 def test_estudante_delete_negativo():
-    assert not estudante_delete(-5)
+    result = estudante_delete(-5)
+    yield result
+    assert not result
 
 def test_estudante_delete_positivo():
-    assert estudante_delete(10)
-
+    result = estudante_delete(10)
+    yield result
+    assert result
